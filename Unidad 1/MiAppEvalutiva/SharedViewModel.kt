8a7ea4b8f0bccbd869f1636fa02f7977b842ef2b
Gustavo Fory <145512440+GForym@users.noreply.github.com>
2025-03-29 13:06:20 -0500

    Create SharedViewModel.kt

---
+++ Unidad 1/MiAppEvalutiva/SharedViewModel.kt
@@ -0,0 +1,14 @@
+package com.gustavo.miappevaluativa
+import androidx.compose.ui.graphics.Color
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.LiveData
+
+class SharedViewModel : ViewModel() {
+    private val _backgroundColor = MutableLiveData(Color.White)
+    val backgroundColor: LiveData<Color> = _backgroundColor
+
+    fun setBackgroundColor(color: Color) {
+        _backgroundColor.value = color
+    }
+}
